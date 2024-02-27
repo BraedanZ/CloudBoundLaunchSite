7804f809b108b6d602228990407be7d3ed09ef95
BraedanZ <60045806+BraedanZ@users.noreply.github.com>
2024-02-27 10:11:53 -0500

    start making launch site

---
+++ mailList.php
@@ -0,0 +1,24 @@
+<?php
+
+if(isset($_POST['submit'])) {
+    $mailto = "bzgames.info@gmail.com";
+    $fromEmail = $_POST['email'];
+
+    $message = "Client Email";
+
+    $confirmationMessage = "Thanks for signing up for the Cloud Bound waiting list.  You will be notified when the game is released!" . "/n/n"
+    . "Regards," . "/n"
+    . "Braedan";
+
+    $headers = "From:  " . $fromEmail;
+    $headers2 = "From:  " . $mailto;
+
+    $result1 = mail($mailto, $subject, $message, $headers);
+    $result2 = mail($fromEmail, $subject2, $confirmationMessage, $headers2);
+
+    if ($result1 && $result2) {
+        $success = "Your message was sent successfully!";
+    } else {
+        $failed = "Sorry!  Message was not sent, try again later.";
+    }
+}
